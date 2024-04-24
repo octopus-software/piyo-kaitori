4966d1f41f15e9dfc40f476abd5e4f722d25ea36
WankoNoGithub <bassist.ryu@gmail.com>
2024-04-24 17:30:09 +0900

    タイトルを削除しました

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,7 +55,6 @@
                     </header>
 
                     <main class="mt-6">
-                        <h1>Hello World! We are piyo-kaitori !!!!! POW!!!!</h1>
                         <div class="grid gap-6 lg:grid-cols-2 lg:gap-8">
                             <a
                                 href="https://laravel.com/docs"
