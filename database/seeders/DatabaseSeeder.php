13a3c1d214307d944b9c88863809d7293f4cd052
taiga <taigabox224@gmail.com>
2024-06-02 18:24:51 +0900

    シーダー追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Database\Seeders\Dummy\UserSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // シーダーを呼び出す
+        $this->call([
+            // Dummy\UserSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
