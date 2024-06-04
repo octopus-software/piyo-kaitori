60bba56cafcad29268afa6d9355145f45da7bb0f
taiga <taigabox224@gmail.com>
2024-06-04 15:47:30 +0900

    不要ファイルの削除

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use App\Http\Controllers\Home\HomeController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -23,7 +22,5 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-Route::get('/home', HomeController::class );
 
 require __DIR__.'/auth.php';
