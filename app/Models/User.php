a111be61ccb22c6d24e3606e2ed1739902fedd50
WankoNoGithub <164289873+WankoNoGithub@users.noreply.github.com>
2024-06-08 15:56:57 +0900

    Revert "シーダーとリレーションの追加"

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -45,11 +44,4 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
-
-    // リレーション設定
-    public function todo(): HasMany
-    {
-        return $this->hasMany(PurchaseTarget::class);
-    }
-
 }
