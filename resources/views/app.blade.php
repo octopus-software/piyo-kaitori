cb207161c6f7c7d220c85f9799a09ef9a177d758
taiga <taigabox224@gmail.com>
2024-06-17 00:49:05 +0900

    tailwind含むCSS周りの修正

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,9 +9,9 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
         <!-- Scripts -->
         @routes
+        @vite('resources/css/app.css')
         @vite(['resources/js/app.ts', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
