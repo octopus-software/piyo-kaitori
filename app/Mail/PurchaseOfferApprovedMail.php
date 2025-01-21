87d40a62cf9b8a8a40f0b2c447da925ac4da7dd9
WankoNoGithub <bassist.ryu@gmail.com>
2025-01-21 09:33:12 +0900

    #13 「未承認」→「承認済み」ステータス変更時にクライアントにお知らせメールを送信する

---
+++ app/Mail/PurchaseOfferApprovedMail.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class PurchaseOfferApprovedMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $purchase_offer_id;
+    public $purchase_offer;
+    public $purchase_targets;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($purchase_offer_id,$purchase_offer,$purchase_targets)
+    {
+        $this->purchase_offer_id = $purchase_offer_id;
+        $this->purchase_offer = $purchase_offer;
+        $this->purchase_targets = $purchase_targets;
+
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: '【ぴよ買取】買取オファーが承認されました ※郵送買取は元払いでご発送ください',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.purchase_offer_approved_mail',
+            with: [
+                'purchase_offer_id' => $this->purchase_offer_id,
+                'purchase_offer' => $this->purchase_offer,
+                'purchase_targets' => $this->purchase_targets,
+            ]
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
