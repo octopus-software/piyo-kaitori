def2b3e3b15aec123e882155b8c883444dcf77cc
taiga <taigabox224@gmail.com>
2025-07-14 16:17:44 +0900

    #fix 初期設定流し込み

--- database/seeders/Dummy/UserSeeder.php
+++ database/seeders/Dummy/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders\Dummy;
 
+use App\Models\Company;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -13,10 +14,29 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::query()->create([
-            'name' => 'Test User',
-            'email' => 'test1@example.com',
+            'id' => 1,
+            'name' => 'ぴよぴよカンパニー',
+            'email' => 'hickcompany20190905@gmail.com',
             'password' => bcrypt('password'),
             'role' => User::USER_ROLE['admin'],
+        ]);
+
+        User::query()->create([
+            'name' => 'テスト用ユーザー',
+            'email' => 'test@example.com',
+            'password' => bcrypt('password'),
+            'role' => User::USER_ROLE['client'],
+        ]);
+
+        Company::query()->create([
+            'user_id' => 1,
+            'name' => 'ぴよぴよカンパニー',
+            'email' => 'info@hiyoco-kaitori.com',
+            'address' => '千葉県松戸市松戸１２２８−１ー５F',
+            'representative_name' => '橋本達也',
+            'tel' => '07083857096',
+            'secondhand_dealer_license_number' => '第401300000065号',
+            'send_address' => '千葉県松戸市松戸１２２８−１ー５F'
         ]);
     }
 }
